1c5ce6d49156d187b744df6d773fe13d25c07637
rotatik <tdkarasev@mail.ru>
2025-04-01 18:21:15 +0300

    testoviy commit                                                                                                         ~                                                                                                                       ~ testoviy commit

---
+++ app/database.py
@@ -0,0 +1,18 @@
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DB_HOST= "localhost"
+DB_PORT= 5432
+DB_USER= "postgress"
+DB_PASS= "postgress"
+DB_NAME= "postgress"
+
+DATABASE_URL =f"postgresql+asyncpg://[{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, class_= AsyncSession, expire_on_commit = False)
+
+class Base(DeclarativeBase):
+    pass
--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
-
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get('/challanges')
+def challang(message: str, type: str = None, day: int = None, hours = None):
+    return {'message': message}
+
+class ToDoItem(BaseModel):
+    challange: str
+    day: date
+    type: str
+
+@app.post('/to_do')
+def todo(item: ToDoItem):
+    return {'message': 'ToDo item created successfully', 'item': item}
