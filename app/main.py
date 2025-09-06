c2a5a23fa6ac3345e3dc5f05bde7f0939fa4bd82
rotatik <tdkarasev@mail.ru>
2025-09-06 16:40:20 +0300

    perenos

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 from datetime import date
 from pydantic import BaseModel
+from app.task.router import router as router_task
 
 app = FastAPI()
 
-@app.get('/challanges')
-def challang(message: str, type: str = None, day: int = None, hours = None):
-    return {'message': message}
+app.include_router(router_task)
 
-class ToDoItem(BaseModel):
-    challange: str
-    day: date
-    type: str
 
-@app.post('/to_do')
-def todo(item: ToDoItem):
-    return {'message': 'ToDo item created successfully', 'item': item}
+    
